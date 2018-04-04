5433ea17aef6499b769da256a1b197fb4e89fc4b
jetwaves@office <jetwaves@office>
2018-04-04 19:10:58 +0800

    integrate left panel to main page

--- web-viewer/routes/index.js
+++ web-viewer/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/nav', function(req, res, next) {
+    res.render('nav.html', { title: 'Express' });
+});
+
+
+router.get('/nav', function(req, res, next) {
+    res.render('nav.html', { title: 'Express' });
+});
+
 
 module.exports = router;
