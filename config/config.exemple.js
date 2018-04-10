d6a4405b5fba8763298a8180bc8029dc76446061
jetwaves@office <jetwaves@office>
2018-04-10 18:58:45 +0800

    maj: - extract sort param. - extract mongodb config

---
+++ config/config.exemple.js
@@ -0,0 +1,50 @@
+/*   环境变量和配置   */
+"use strict";
+var config = {
+    // api :{          // not using
+    //     app_port    : 3000,
+    //     temp_file_folder : 'temp',
+    //     api_host: 'http://localhost',
+    //     api_port: 80,
+    //     api_url : '/api',            /* 这个url在模块对外暴露前会被拼接成完整的  http://hostname:port/uri 格式   */
+    // },
+
+    // ui  :   {
+    //     company             : '',                   //  company abbreviation str
+    //     page_title          : '',                   //  进入系统后浏览器标签的title，可以自定义
+    //     company_cns_name    : '',                   //  公司中文名
+    //     company_web_site    : '',                   //  公司网站
+    //     company_cns_abbrev  : '',                   //  公司中文简称
+    //     ICP_beian_sn        : '',                   //  ICP备案号
+    //     copy_right_year     : '',                   //  版权有效期
+    //     title_pic           : ''                    //  暂时没用
+    // },
+
+    system  :   {
+        env             : 'dev',                       //   [dev, int, prod]
+        session_secret  : 'local session secret for session store',
+    },
+
+    admin: {        /*  管理后台管理员相关配置  */
+
+    },
+
+    mongoDB:{                   /*  MongoDB 数据库连接参数   */
+        host     : 'localhost',
+        port     : '27017',
+        user     : 'git',
+        password : '123456',
+        db       : 'gitperf',
+        collection: 'test'
+    }
+};
+
+// if(config.api_port == 80){
+//     config.api_url = config.api_host + '/api';
+// }else {
+//     config.api_url = config.api_host + ':' + config.api_port  + config.api_url;
+// }
+
+
+
+module.exports = config;
--- web-viewer/routes/TeamPerformance.js
+++ web-viewer/routes/TeamPerformance.js
@@ -6,6 +6,9 @@ require('./common');
 let moment = require('moment');
 let util = require('../../helpers/util.js');
 
+let config = require('../../config/config.js');
+
+
 router.get('/list',teamPerformanceList);
 router.post('/search',search);
 
@@ -23,7 +26,7 @@ function search(req,res,next){
     if(start_ts && !end_ts) queryParam.date = { $gte: start_ts };
     if(!start_ts && end_ts) queryParam.date = { $lte: end_ts };
     if(start_ts && end_ts)  queryParam.date = { $gte: start_ts, $lte: end_ts };
-    if(req.body.author) queryParam.author = queryParam.author = eval("/" + req.body.author + "/i");
+    if(req.body.author) queryParam.author = queryParam.author = eval("/" + req.body.author + "/i"); // query with 'like' as SQL
     if(req.body.project) queryParam.project = req.body.project;
     if(req.body.branch) queryParam.branch = req.body.branch;
     if(req.body.msg) queryParam.msg = queryParam.msg = eval("/" + req.body.msg + "/i");;
@@ -34,23 +37,15 @@ function search(req,res,next){
     if(req.body.pages){
         paginate = util.parsePaginateRule(req.body.pages, 50);       // 把分页规则变成  skip 和 take,  默认每页50条
     }
-    // console.log("\r\n"+moment().format('Y/MM/DD HH:mm:ss\t\t\t\t')+__filename);
-    // console.log('┏---- INFO: ----- start [paginate @ ] -----');console.dir(paginate);console.log('┗---- INFO: -----  end  [paginate @ ] -----');
-    let sortParam = undefined;
-    if(req.body.sortParam){
-        let arr = req.body.sortParam.split(':');
-        switch(arr[0]){                     // js不允许用变量做kv对的key，只能这么写
-            case 'date':    sortParam = {'date':       parseInt(arr[1])};   break;
-            case 'author':  sortParam = {'author':     parseInt(arr[1])};   break;
-            case 'msg':     sortParam = {'msg':        parseInt(arr[1])};   break;
-            case 'project': sortParam = {'project':    parseInt(arr[1])};   break;
-            case 'branch':  sortParam = {'branch':     parseInt(arr[1])};   break;
-            case 'hash':    sortParam = {'hash':       parseInt(arr[1])};   break;
-        }
-    }
+    let sortParam = {};
+    let arr = req.body.sortParam.split(':');
+    sortParam[arr[0]] = parseInt(arr[1]);
 
     let mongojs = require('mongojs');
-    let db = mongojs('git:123456@127.0.0.1/gitperf', ['test']);
+    // let db = mongojs('userName:userPassword@hostName/DatabaseName', ['collectionName']);  // like // let db = mongojs('git:123456@127.0.0.1/gitperf', ['test']);
+    let db = mongojs( config.mongoDB.user + ':' + config.mongoDB.password + '@'
+                    + config.mongoDB.host + ':' + config.mongoDB.port     + '/'
+                    + config.mongoDB.db, [ config.mongoDB.collection]);
     let col = db.collection('test');
     // console.log("\r\n"+moment().format('Y/MM/DD HH:mm:ss\t\t\t\t')+__filename);
     // console.log('┏---- INFO: ----- start [queryParam @ ] -----');console.dir(queryParam);console.log('┗---- INFO: -----  end  [queryParam @ ] -----');
